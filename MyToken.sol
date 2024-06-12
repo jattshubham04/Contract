0b285f50250c42f9821992f39f620ef2babf38f2
jattshubham04 <122143026+jattshubham04@users.noreply.github.com>
2024-06-12 23:15:34 +0530

    MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -2,25 +2,25 @@
 pragma solidity 0.8.18;
 
 contract MyToken {
-
     // Public variables to store the details about the coin
     string public name = "MyToken";
     string public symbol = "MTK";
-    uint256 public totalSupply = 0;
+    uint256 public totalSupply;
 
     // Mapping of addresses to balances
     mapping(address => uint256) public balances;
 
-    // Mint function to increase the total supply and the balance of the sender address
-    function mint(address _to, uint256 _amount) public {
-        totalSupply += _amount;
-        balances[_to] += _amount;
+    // Mint function to create new tokens
+    function mint(address _to, uint256 _value) public {
+        totalSupply += _value;
+        balances[_to] += _value;
     }
 
-    // Burn function to decrease the total supply and the balance of the sender address
-    function burn(address _from, uint256 _amount) public {
-        require(balances[_from] >= _amount, "Insufficient balance to burn");
-        totalSupply -= _amount;
-        balances[_from] -= _amount;
+    // Burn function to destroy tokens
+    function burn(address _from, uint256 _value) public {
+        require(balances[_from] >= _value, "Insufficient balance to burn");
+        totalSupply -= _value;
+        balances[_from] -= _value;
     }
 }
+
